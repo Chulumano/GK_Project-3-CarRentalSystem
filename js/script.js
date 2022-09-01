aeb54c15f3d47f391921d428c1c0e78dd244c841
Chulumanco Buhle Nkwindana <81573153+Chulumano@users.noreply.github.com>
2022-09-01 18:45:37 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,152 +1,179 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
 }
 
-document.querySelector('#login-btn').onclick = () => {
-    document.querySelector('.login-form-container').classList.toggle('active');
+document.querySelector('#login-btn').onclick = () =>{
+  document.querySelector('.login-form-container').classList.toggle('active');
+  
 }
 
-document.querySelector('#close-login-form').onclick = () => {
-    document.querySelector('.login-form-container').classList.remove('active');
+
+
+document.querySelector('#close-login-form').onclick = () =>{
+  document.querySelector('.login-form-container').classList.remove('active');
 }
 
-window.onscroll = () => {
 
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
 
-    if (window.scrollY > 0) {
-        document.querySelector('.header').classList.add('active');
-    } else {
-        document.querySelector('.header').classList.remove('active');
-    };
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+
+
+
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  }else{
+    document.querySelector('.header').classList.remove('active');
+  };
 
 };
 
-document.querySelector('.home').onmousemove = (e) => {
+document.querySelector('.home').onmousemove = (e) =>{
 
-    document.querySelectorAll('.home-parallax').forEach(elm => {
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
 
-        let speed = elm.getAttribute('data-speed');
+    let speed = elm.getAttribute('data-speed');
 
-        let x = (window.innerWidth - e.pageX * speed) / 90;
-        let y = (window.innerHeight - e.pageY * speed) / 90;
+    let x = (window.innerWidth - e.pageX * speed) / 90;
+    let y = (window.innerHeight - e.pageY * speed) / 90;
 
-        elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+    elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
 
-    });
+  });
 
 };
 
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 
-document.querySelector('.home').onmouseleave = (e) => {
+function verifyPassword() {  
+  var pw = document.getElementById("password").value;  
+  //check empty password field  
+  if(pw == "") {  
+     document.getElementById("message").innerHTML = "**Fill the password please!";  
+     return false;  
+  }  
+   
+ //minimum password length validation  
+  if(pw.length < 8) {  
+     document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
+     return false;  
+  }  
+  
+//maximum length of password validation  
+  if(pw.length > 15) {  
+     document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";  
+     return false;  
+  } else {  
+     alert("Password is correct");  
+  }  
+}  
 
-    document.querySelectorAll('.home-parallax').forEach(elm => {
 
-        elm.style.transform = `translateX(0px) translateY(0px)`;
+document.querySelector('.home').onmouseleave = (e) =>{
 
-    });
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+
+    elm.style.transform = `translateX(0px) translateY(0px)`;
+
+  });
 
 };
 
 var swiper = new Swiper(".vehicles-slider", {
-    grabCursor: true,
-    centeredSlides: true,
-    spaceBetween: 20,
-    loop: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+    768: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
+    1024: {
+      slidesPerView: 3,
     },
+  },
 });
 
 var swiper = new Swiper(".featured-slider", {
-    grabCursor: true,
-    centeredSlides: true,
-    spaceBetween: 20,
-    loop: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+    768: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
+    1024: {
+      slidesPerView: 3,
     },
+  },
 });
 
 var swiper = new Swiper(".review-slider", {
-    grabCursor: true,
-    centeredSlides: true,
-    spaceBetween: 20,
-    loop: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+    768: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
+    1024: {
+      slidesPerView: 3,
     },
-});
-
-////
-const faqHeaders = document.querySelectorAll(".faqs-container .faq-header");
-
-faqHeaders.forEach((header, i) => {
-    header.addEventListener("click", () => {
-        header.nextElementSibling.classList.toggle("active");
-
-        const open = header.querySelector(".open");
-        const close = header.querySelector(".close");
-
-        if (header.nextElementSibling.classList.contains("active")) {
-            open.classList.remove("active");
-            close.classList.add("active");
-        } else {
-            open.classList.add("active");
-            close.classList.remove("active");
-        }
-    });
+  },
 });
